f1b6111d3e52bab01a923bbc920b6f0a29da4031
Maria Belova <mariabelova.dev@gmail.com>
2021-01-28 23:16:41 +0300

    Made adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
   loop: true,
   navigation: {
-    nextEl: '.arrow',
+    nextEl: ".arrow",
   },
+  breakpoints: {
+    // when window width is >= 320px
+    540: {
+      slidesPerView: 2,
+    },
+  }
 });
+
+var menuButton = document.querySelector(".menu-btn");
+var menu = document.querySelector(".header");
+menuButton.addEventListener("click", function () {
+  menuButton.classList.toggle('menu-btn-active');
+  menu.classList.toggle('header-active');
+})
